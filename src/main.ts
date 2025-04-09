bfa41d8f3bba6f5e2333f8aed0467e5d3f2d26c5
Nguyễn Tùng Dương <duongnt.b24cc088@stu.ptit.edu.vn>
2025-04-09 21:55:44 +0700

    change-port-default

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupSwagger } from './swagger';
+import { setupSwagger } from './config/swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
 
-  await app.listen(process.env.PORT ?? 3030);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

80cdc0bc0089ee36cc274c1061f49b35e4443a3c
Nguyễn Tùng Dương <duongnt.b24cc088@stu.ptit.edu.vn>
2025-04-09 22:29:46 +0700

    add-http-exception.filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
+
+  // Apply the exception filter globally
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }

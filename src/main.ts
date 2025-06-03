f6b643b69bb004e09a173949faa24d05c658430e
Nguyễn Tùng Dương <duongnt.b24cc088@stu.ptit.edu.vn>
2025-06-03 09:29:29 +0700

    change url for cors

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
 
   app.enableCors({
-    origin: 'http://127.0.0.1:5500', // Chỉ cho phép từ domain này
+    origin: 'https://csdl-dien-dan-fe.onrender.com', // Chỉ cho phép từ domain này
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Các phương thức HTTP được cho phép
     credentials: true, // Cho phép gửi cookie/token qua CORS
   });
